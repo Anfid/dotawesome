fa4797445fd52db19493d3a3fd33e77056561c51
Pogretskiy Mikhail <pogretzkiy.mihail@gmail.com>
2018-11-12 07:11:09 +0300

    Add extra tag rules

--- rc.lua
+++ rc.lua
@@ -23,6 +23,7 @@ naughty.config.defaults['icon_size'] = 64
 -- User library
 local cosy = require("cosy")
 local global = require("global")
+local rules = require("rules")
 
 awesome.set_preferred_icon_size(global.panel_size)
 
@@ -239,58 +240,7 @@ root.buttons(cosy.bindings.mouse.global)
 root.keys(cosy.bindings.keyboard.global)
 -- }}}
 
--- {{{ Rules
--- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-    -- All clients will match this rule.
-    { rule = { },
-      properties = { border_width = beautiful.border_width,
-                     border_color = beautiful.border_normal,
-                     focus = awful.client.focus.filter,
-                     raise = true,
-                     keys = cosy.bindings.keyboard.client,
-                     buttons = cosy.bindings.mouse.client,
-                     screen = awful.screen.preferred,
-                     placement = awful.placement.no_overlap+awful.placement.no_offscreen
-     }
-    },
-
-    -- Floating clients.
-    { rule_any = {
-        instance = {
-          "DTA",  -- Firefox addon DownThemAll.
-          "copyq",  -- Includes session name in class.
-        },
-        class = {
-          "Arandr",
-          "Gpick",
-          "Kruler",
-          "MessageWin",  -- kalarm.
-          "Sxiv",
-          "Wpa_gui",
-          "pinentry",
-          "veromix",
-          "xtightvncviewer"},
-
-        name = {
-          "Event Tester",  -- xev.
-        },
-        role = {
-          "AlarmWindow",  -- Thunderbird's calendar.
-          "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
-        }
-      }, properties = { floating = true }},
-
-    -- Add titlebars to normal clients and dialogs
-    { rule_any = {type = { "normal", "dialog" }
-      }, properties = { titlebars_enabled = true }
-    },
-
-    -- Set Firefox to always map on the tag named "2" on screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { screen = 1, tag = "2" } },
-}
--- }}}
+awful.rules.rules = rules
 
 -- {{{ Functions
 -- Toggle titlebar on or off depending on s. Creates titlebar if it doesn't exist
---
+++ rules.lua
@@ -0,0 +1,85 @@
+---------------------------------------------------------------------------
+--- Rules
+--
+-- Awesome tag settings
+---------------------------------------------------------------------------
+
+local awful = require("awful")
+local beautiful = require("beautiful")
+local cosy = require("cosy")
+
+local screen = _G.screen
+
+-- Rules to apply to new clients (through the "manage" signal).
+local rules = {
+    -- All clients will match this rule.
+    {
+        rule = {},
+        properties = {
+            border_width = beautiful.border_width,
+            border_color = beautiful.border_normal,
+            focus = awful.client.focus.filter,
+            raise = true,
+            keys = cosy.bindings.keyboard.client,
+            buttons = cosy.bindings.mouse.client,
+            screen = awful.screen.preferred,
+            placement = awful.placement.no_overlap+awful.placement.no_offscreen
+        }
+    },
+
+    -- Floating clients.
+    {
+        rule_any = {
+            instance = {
+                "DTA",  -- Firefox addon DownThemAll.
+                "copyq",  -- Includes session name in class.
+            },
+
+            class = {
+                "Arandr",
+                "Gpick",
+                "Kruler",
+                "MessageWin",  -- kalarm.
+                "Sxiv",
+                "Wpa_gui",
+                "pinentry",
+                "veromix",
+                "xtightvncviewer"},
+
+            name = {
+                "Event Tester",  -- xev.
+            },
+
+            role = {
+                "AlarmWindow",  -- Thunderbird's calendar.
+                "pop-up",       -- e.g. Google Chrome's (detached) Developer Tools.
+            }
+        },
+        properties = { floating = true }
+    },
+
+    -- Add titlebars to normal clients and dialogs
+    {
+        rule_any = {
+            type = { "normal", "dialog" }
+        },
+        properties = { titlebars_enabled = true }
+    },
+
+    -- Assign default tags
+    {
+        rule_any = {
+            class = {"Waterfox", "Firefox"}
+        },
+        properties = { screen = 1, tag = "1" }
+    },
+
+    {
+        rule_any = {
+            class = {"TelegramDesktop", "Slack"}
+        },
+        properties = { screen = screen.count()>1 and 2 or 1, tag = screen.count() > 1 and "1" or "4" }
+    },
+}
+
+return rules
