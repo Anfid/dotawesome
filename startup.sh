6a082053ecdc3c4841eb243be0fc7dc37b9335fe
Mikhail Pogretskiy <mpogret@mera.ru>
2019-01-10 15:19:14 +0300

    Add conky to startup script

--- startup.sh
+++ startup.sh
@@ -7,25 +7,22 @@ function run-unique {
   fi
 }
 
-function awful-spawn-unique {
-  PNAME=`echo $3 | cut -c-15`
-  if ! pgrep $PNAME;
-  then
-    awesome-client "
-      awful=require(\"awful\");
-      s = $1;
-      t = $2;
-      awful.spawn(\"${@:3}\", {
-        screen    = s,
-        tag       = t,
-      })"
-  fi
-}
+export LC_ALL=C
 
 setxkbmap -layout us,ru -option 'grp:alt_shift_toggle'
 run-unique compton
-run-unique dunst -conf $HOME/.config/dunst/dunstrc
-run-unique cava -p $HOME/.config/cava/raw
+run-unique dunst -conf "$HOME/.config/dunst/dunstrc"
+run-unique cava -p "$HOME/.config/cava/raw"
 
-awful-spawn-unique 'math.min(2, screen.count())' 'screen.count() == 2 and 1 or 4' telegram-desktop
-awful-spawn-unique 'math.min(2, screen.count())' 'screen.count() == 2 and 1 or 4' slack
+run-unique telegram-desktop
+run-unique slack
+
+if ! pgrep "conky"
+then
+  conky -c "$HOME/.conky/rings" & # the main conky with rings
+  sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
+  conky -c "$HOME/.conky/cpu" &
+  sleep 1
+  conky -c "$HOME/.conky/mem" &
+  conky -c "$HOME/.conky/notes" &
+fi
