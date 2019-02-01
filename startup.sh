39f3ac99abaa0eda85d4af7d862bd6152afcd6eb
Mikhail Pogretskiy <mpogret@mera.ru>
2019-02-01 22:43:36 +0300

    Remove slack startup. Reduce conky delay

--- startup.sh
+++ startup.sh
@@ -15,12 +15,11 @@ run-unique dunst -conf "$HOME/.config/dunst/dunstrc"
 run-unique cava -p "$HOME/.config/cava/raw"
 
 run-unique telegram-desktop
-run-unique slack
 
 if ! pgrep "conky"
 then
   conky -c "$HOME/.conky/rings" & # the main conky with rings
-  sleep 8 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
+  sleep 3 #time for the main conky to start; needed so that the smaller ones draw above not below (probably can be lower, but we still have to wait 5s for the rings to avoid segfaults)
   conky -c "$HOME/.conky/cpu" &
   sleep 1
   conky -c "$HOME/.conky/mem" &
