946341147bc10b3954c5fd1482efa331b0114064
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2019-03-11 13:55:58 +0300

    Start implementing volume widget for horizontal bars

--- cosy/widget/panel/volume.lua
+++ cosy/widget/panel/volume.lua
@@ -22,11 +22,84 @@ volume.mt = {}
 
 volume.defaults = {
     timeout = 3,
+    orientation = "vertical",
     size = 100,
     bar_width = 3,
     indicator_width = 2,
     indicator_offset = 5,
 }
+
+local function draw_vertical(widget, context, cr, width, height)
+    if widget.vol >= 100 then
+        cr:set_source(gears.color(beautiful.color_red .. "a0"))
+    elseif widget.vol >= 75 then
+        cr:set_source(gears.color(beautiful.color_yellow .. "a0"))
+    else
+        cr:set_source(gears.color(beautiful.color_green .. "a0"))
+    end
+    cr:set_line_width(widget.bar_width)
+
+    local x = width / 2
+    local val = height - (widget.vol <= 100 and widget.vol or 100) * (widget.size / 100)
+
+    local off = widget.offset
+
+    cr:move_to(x, height + off)
+    cr:line_to(x, val + off)
+    cr:stroke()
+
+    cr:set_source(gears.color(beautiful.bg_focus .. "a0"))
+    cr:move_to(x, val + off)
+    cr:line_to(x, 0 + off)
+    cr:stroke()
+
+    cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
+    cr:set_line_width(widget.indicator_width)
+    local i = widget.bar_width / 2 + widget.indicator_offset
+    cr:move_to(x - i, val + off)
+    cr:line_to(x + i, val + off)
+    cr:stroke()
+end
+
+local function draw_horizontal(widget, context, cr, width, height)
+    if widget.vol >= 100 then
+        cr:set_source(gears.color(beautiful.color_red .. "a0"))
+    elseif widget.vol >= 75 then
+        cr:set_source(gears.color(beautiful.color_yellow .. "a0"))
+    else
+        cr:set_source(gears.color(beautiful.color_green .. "a0"))
+    end
+    cr:set_line_width(widget.bar_width)
+
+    local y = height / 2
+    local val = width - (widget.vol <= 100 and widget.vol or 100) * (widget.size / 100)
+
+    local off = widget.offset
+
+    cr:move_to(width + off, y)
+    cr:line_to(val + off, y)
+    cr:stroke()
+
+    cr:set_source(gears.color(beautiful.bg_focus .. "a0"))
+    cr:move_to(val + off, y)
+    cr:line_to(0 + off, y)
+    cr:stroke()
+
+    cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
+    cr:set_line_width(widget.indicator_width)
+    local i = widget.bar_width / 2 + widget.indicator_offset
+    cr:move_to(val + off, x - i)
+    cr:line_to(val + off, x + i)
+    cr:stroke()
+end
+
+local function fit_vertical(widget, context, width, height)
+    return width, widget.size
+end
+
+local function fit_horizontal(widget, context, width, height)
+    return widget.size, height
+end
 
 function volume.new(properties)
     local properties = gears.table.join(volume.defaults, properties or {})
@@ -35,40 +108,14 @@ function volume.new(properties)
     volume_widget.shown = false
     volume_widget.offset = 0
 
-    function volume_widget:fit(context, width, height)
-        return width, self.size
-    end
-
-    function volume_widget:draw(context, cr, width, height)
-        if self.vol >= 100 then
-            cr:set_source(gears.color(beautiful.color_red .. "a0"))
-        elseif self.vol >= 75 then
-            cr:set_source(gears.color(beautiful.color_yellow .. "a0"))
-        else
-            cr:set_source(gears.color(beautiful.color_green .. "a0"))
-        end
-        cr:set_line_width(self.bar_width)
-
-        local x = width / 2
-        local val = height - (self.vol <= 100 and self.vol or 100) * (self.size / 100)
-
-        local off = self.offset
-
-        cr:move_to(x, height + off)
-        cr:line_to(x, val + off)
-        cr:stroke()
-
-        cr:set_source(gears.color(beautiful.bg_focus .. "a0"))
-        cr:move_to(x, val + off)
-        cr:line_to(x, 0 + off)
-        cr:stroke()
-
-        cr:set_source(gears.color(beautiful.fg_normal .. "a0"))
-        cr:set_line_width(self.indicator_width)
-        local i = self.bar_width / 2 + self.indicator_offset
-        cr:move_to(x - i, val + off)
-        cr:line_to(x + i, val + off)
-        cr:stroke()
+    if volume_widget.orientation == "vertical" then
+        volume_widget.draw = draw_vertical
+        volume_widget.fit = fit_vertical
+    elseif volume_widget.orientation == "horizontal" then
+        volume_widget.draw = draw_horizontal
+        volume_widget.fit = fit_horizontal
+    else
+        error("Wrong volume widget orientation")
     end
 
     function volume_widget:update()
@@ -92,7 +139,6 @@ function volume.new(properties)
     volume_widget.timeout = gears.timer.start_new(
         volume_widget.timeout,
         function()
-            dbg.notify("expired")
             volume_widget.shown = false
             volume_widget.animation_timer:again()
             return false
